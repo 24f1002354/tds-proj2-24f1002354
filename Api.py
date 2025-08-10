2197c03b9884699885eb9654da276a4ee09eded5
Christian Julius <julius.cj@gmail.com>
2025-08-10 11:05:40 +1000

    WSGI compatibility

--- Api.py
+++ Api.py
@@ -44,5 +44,8 @@ def api():
     
     return jsonify(answer)
 
+# WSGI entrypoint for httpd/mod_wsgi
+application = app
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9004, debug=False)
